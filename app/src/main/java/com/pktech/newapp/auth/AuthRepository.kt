63b99efc8eccce02c9eeaedc6abb2627e1859660
pankaj42991 <pankajkumar42991@gmail.com>
2026-02-05 13:23:17 +0530

    Update AuthRepository.kt

--- app/src/main/java/com/pktech/newapp/auth/AuthRepository.kt
+++ app/src/main/java/com/pktech/newapp/auth/AuthRepository.kt
@@ -1,12 +1,15 @@
 package com.pktech.newapp.auth
 
+import android.app.Application
 import com.google.firebase.auth.FirebaseAuth
 import com.pktech.newapp.NewAppApplication
 import com.pktech.newapp.data.local.entity.EmployeeEntity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class AuthRepository(private val app: NewAppApplication) {
+class AuthRepository(
+    private val application: Application
+) {
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
@@ -14,13 +17,14 @@ class AuthRepository(private val app: NewAppApplication) {
      * Sync Firebase user into Room DB
      */
     suspend fun syncUserWithLocalDb() = withContext(Dispatchers.IO) {
+
         val firebaseUser = auth.currentUser ?: return@withContext
 
         val email = firebaseUser.email ?: return@withContext
         val name = firebaseUser.displayName ?: email.substringBefore("@")
 
-        val db = app.database
-        val employeeDao = db.employeeDao()
+        val app = application as NewAppApplication
+        val employeeDao = app.database.employeeDao()
 
         val existing = employeeDao.getByEmail(email)
 
