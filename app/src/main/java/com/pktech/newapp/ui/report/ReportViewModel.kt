ed1904ec522aba2cfe4c3a9e55cb6cb5f9336466
pankaj42991 <pankajkumar42991@gmail.com>
2026-02-06 08:25:53 +0530

    Update ReportViewModel.kt

--- app/src/main/java/com/pktech/newapp/ui/report/ReportViewModel.kt
+++ app/src/main/java/com/pktech/newapp/ui/report/ReportViewModel.kt
@@ -1,44 +1,22 @@
 package com.pktech.newapp.ui.report
 
-import android.content.Context
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.pktech.newapp.data.local.entity.ShiftEntity
 import com.pktech.newapp.data.repository.EmployeeRepository
-import com.pktech.newapp.pdf.PdfGenerator
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.io.File
 
 class ReportViewModel(
     private val repository: EmployeeRepository
 ) : ViewModel() {
 
-    fun getAllShifts() = repository.getAllShifts()
+    private val _allShifts = MutableLiveData<List<ShiftEntity>>()
+    val allShifts: LiveData<List<ShiftEntity>> = _allShifts
 
-    fun generateAdminMonthlyReport(
-        context: Context,
-        month: String,
-        onResult: (File) -> Unit,
-        onError: (Throwable) -> Unit = {}
-    ) {
-        viewModelScope.launch(Dispatchers.IO) {
-            try {
-                val shifts = repository.getAdminMonthlyReport(month)
-
-                val grouped = shifts.groupBy {
-                    it.date.substring(0, 7) // yyyy-MM
-                }
-
-                val pdf = PdfGenerator.generateAdminMonthlyReport(
-                    context,
-                    month,
-                    grouped
-                )
-                onResult(pdf)
-            } catch (e: Exception) {
-                onError(e)
-            }
+    fun loadAllShifts() {
+        viewModelScope.launch {
+            _allShifts.postValue(
+                repository.getAllShifts()
+            )
         }
     }
 }
